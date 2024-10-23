d5b09451b15cfcc205f90b81bd75a847386d0617
Tom <dev@luckyft.com>
2024-10-23 08:36:05 +0800

    ✨ add creatSnake

--- contracts/recipes/CryptoSnake.sol
+++ contracts/recipes/CryptoSnake.sol
@@ -11,9 +11,17 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+struct TokenStats {
+    string nickname;
+    uint256 totalScore;
+    uint256 gamesPlayed;
+}
+
 /// @title Crypto Snake
 /// @dev A contract that work as a game plaform of snake game
-contract CryptoSnake is CollectionMinter, TokenMinter, TokenManager, AddressValidator, UniqueNFT {
+contract CryptoSnake is CollectionMinter, TokenMinter, TokenManager, AddressValidator, ERC721, ERC721URIStorage {
+    mapping(uint256 tokenId => TokenStats) private s_tokenStats;
+
     /// @dev Address of the NFT collection. Created at the deploy time.
     address private immutable COLLECTION_ADDRESS;
     /// @dev This contract mints a snake game collection in the constructor.
@@ -32,51 +40,23 @@ contract CryptoSnake is CollectionMinter, TokenMinter, TokenManager, AddressVali
 
     receive() external payable {}
 
-    function createSnake(CrossAddress memory _owner) external {
-        // For simplicity, we have only 2 predefined images, type 1 or type 2.
-        // Each player receives a pseudo-random token breed.
-        uint32 randomTokenBreed = _getPseudoRandom(BREEDS, 1);
-
+    function createSnake(CrossAddress memory _owner, string memory _nickname) external payable {
         // Construct token image URL.
-        string memory randomImage = string.concat(
-            s_generationIpfs[0],
-            "monster-",
-            Converter.uint2str(randomTokenBreed),
-            ".png"
-        );
+        string memory img = "https://crypto-snake.vercel.app/logo.png";
 
         Attribute[] memory attributes = new Attribute[](4);
         // Each NFT has 3 traits. These traits are mutated when the `_fight` method is invoked.
-        attributes[0] = Attribute({trait_type: "Experience", value: "0"});
-        attributes[1] = Attribute({trait_type: "Victories", value: "0"});
-        attributes[2] = Attribute({trait_type: "Defeats", value: "0"});
-        attributes[3] = Attribute({trait_type: "Generation", value: "0"});
+        attributes[0] = Attribute({trait_type: "Nickname", value: _nickname});
+        attributes[1] = Attribute({trait_type: "Total Score", value: "0"});
+        attributes[2] = Attribute({trait_type: "Games Played", value: "0"});
 
-        uint256 tokenId = _createToken(COLLECTION_ADDRESS, randomImage, attributes, _owner);
+        uint256 tokenId = _createToken(COLLECTION_ADDRESS, img, attributes, _owner);
         s_tokenStats[tokenId] = TokenStats({
-            breed: randomTokenBreed,
-            generation: 0,
-            victories: 0,
-            defeats: 0,
-            experience: 0
+            nickname: _nickname,
+            totalScore: 0,
+            gamesPlayed: 0
         });
     }
-
-    /**
-     * @notice Evolves the token to the next generation if it has enough experience.
-     *         The token's image changes upon evolution.
-     * @param _tokenId The ID of the token to evolve.
-     */
-    function evolve(uint256 _tokenId) external onlyTokenOwner(_tokenId, COLLECTION_ADDRESS) {
-        TokenStats memory tokenStats = s_tokenStats[_tokenId];
-        require(tokenStats.experience >= EVOLUTION_EXPERIENCE, "Experience not enough");
-        require(tokenStats.generation == 0, "Already evolved");
-
-        s_tokenStats[_tokenId].generation = 1;
-        _setTrait(COLLECTION_ADDRESS, _tokenId, "Generation", "1");
-        _setImage(_tokenId, false);
-    }
-
 
     /**
      * @dev Function to mint a new collection.
